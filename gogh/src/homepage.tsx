949bb40c79e2ac0ea07dbd9022903ecfa3bbdbce
ManuelMaccou <93491644+ManuelMaccou@users.noreply.github.com>
2024-01-31 10:07:18 -0500

    Update homepage.tsx

--- gogh/src/homepage.tsx
+++ gogh/src/homepage.tsx
@@ -14,15 +14,8 @@ const HomePage = () => {
 
     return (
         <div className="homepage-container">
-            <h1 className="title">Sell anything on Farcaster</h1>
-            <p className="description">
-                Promote your NFTs, products, and services directly in the feed.
-            </p>
-            <div className="image-container">
-            </div>
-            <button onClick={handleGetStartedClick} className="get-started-button">
-                Get Started
-            </button>
+            <h1 className="title">Hang tight. We're fixing some issues. We'll be back ASAP!</h1>
+            
         </div>
     );
 };
