8c7b247defe0f4c9c688339ae34f17d5df83ba6a
ManuelMaccou <93491644+ManuelMaccou@users.noreply.github.com>
2024-02-01 11:18:18 -0500

    Fixed frames

--- backend/routes/frames.js
+++ backend/routes/frames.js
@@ -8,21 +8,21 @@ const Product = require('../models/product');
 async function getProductAndUser(uniqueId, productIndex) {
     const user = await User.findOne({ pageId: uniqueId }).populate('products');
     if (!user || !user.products || user.products.length === 0 || !user.products[productIndex]) {
-        return { product: null, username: null, framesMeta: null };
+        return { product: null, username: null };
     }
 
-    return { product: user.products[productIndex], username: user.username, framesMeta: user.framesMeta };
+    return { product: user.products[productIndex], username: user.username };
 }
 
 router.get('/frame/:uniqueId', async (req, res) => {
     const productIndex = 0; // Always show the first product initially
-    const { product, username, framesMeta } = await getProductAndUser(req.params.uniqueId, productIndex);
+    const { product, username } = await getProductAndUser(req.params.uniqueId, productIndex);
 
     if (!product) {
         return res.status(404).send('No products found');
     }
 
-    res.send(generateFrameHtml(product, username, framesMeta, req.params.uniqueId, productIndex));
+    res.send(generateFrameHtml(product, username, req.params.uniqueId, productIndex));
 });
 
 router.post('/frame/:uniqueId', async (req, res) => {
@@ -30,6 +30,7 @@ router.post('/frame/:uniqueId', async (req, res) => {
         const uniqueId = req.params.uniqueId;
         const buttonIndex = req.body.untrustedData.buttonIndex;
         let productIndex = parseInt(req.query.index) || 0;
+        const { product, username } = await getProductAndUser(uniqueId, productIndex);
 
         // Handling the 'More Info' button
         if (buttonIndex === 3) {
@@ -48,13 +49,13 @@ router.post('/frame/:uniqueId', async (req, res) => {
             productIndex++;
         }
 
-        const { product, username, framesMeta } = await getProductAndUser(uniqueId, productIndex);
+        
 
         if (!product) {
             return res.status(404).send('Product not found');
         }
 
-        res.status(200).send(generateFrameHtml(product, username, framesMeta, uniqueId, productIndex));
+        res.status(200).send(generateFrameHtml(product, username, uniqueId, productIndex));
     } catch (err) {
         console.error('Error in POST /frame/:uniqueId', err);
         res.status(500).send('Internal Server Error');
@@ -62,27 +63,22 @@ router.post('/frame/:uniqueId', async (req, res) => {
 });
 
 // Helper function to generate frame HTML
-function generateFrameHtml(product, username, framesMeta, uniqueId, productIndex) {
+function generateFrameHtml(product, username, uniqueId, productIndex) {
     const postUrl = `${process.env.BASE_URL}/api/frames/frame/${uniqueId}?index=${productIndex}`;
-
-    // Using framesMeta for metadata
-    const title = framesMeta.ogTitle
-    const description = framesMeta.ogDescription
-    const imageUrl = framesMeta.ogImage
 
     return `
         <!DOCTYPE html>
         <html>
         <head>
-            <title>${title}</title>
-                <meta name="description" content="${description}">
+            <title>${username}'s Products</title>
+                <meta name="description" content="${username}'s products">
                 <meta property="og:url" content="${product.url}">
-                <meta property="og:image" content="${imageUrl}">
-                <meta property="fc:frame" content="${framesMeta.fcFrame}" />
+                <meta property="og:image" content="${product.image}">
+                <meta property="fc:frame" content="vNext" />
                 <meta property="fc:frame:post_url" content="${postUrl}">
-                <meta property="fc:frame:image" content="${framesMeta.fcFrameImage}" />
-                <meta property="fc:frame:button:1:" content="${framesMeta.fcFrameButton1}" />
-                <meta property="fc:frame:button:2:" content="${framesMeta.fcFrameButton2}" />
+                <meta property="fc:frame:image" content="${product.ogImage}" />
+                <meta property="fc:frame:button:1:" content="prev" />
+                <meta property="fc:frame:button:2:" content="next" />
                 <meta property="fc:frame:button:3" content="More info" />
                 <meta property="fc:frame:button:3:action" content="post_redirect" />
             </head>
