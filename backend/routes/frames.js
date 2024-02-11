2399bd1b49f8a9f831022287c6050a3e47749aca
ManuelMaccou <93491644+ManuelMaccou@users.noreply.github.com>
2024-02-11 12:23:15 -0500

    Update frames.js

--- backend/routes/frames.js
+++ backend/routes/frames.js
@@ -207,6 +207,7 @@ function generateFrameHtml(product, username, uniqueId, productIndex, frameType 
             <meta property="fc:frame:button:1" content="prev" />
             <meta property="fc:frame:button:2" content="more info" />
             <meta property="fc:frame:button:3" content="next" />
+            <meta property="fc:frame:image:aspect_ratio" content="1:1" />
         `;
     }
 
@@ -221,7 +222,6 @@ function generateFrameHtml(product, username, uniqueId, productIndex, frameType 
             <meta property="fc:frame" content="vNext" />
             <meta property="fc:frame:post_url" content="${postUrl}">
             <meta property="fc:frame:image" content="${frameContent}">
-            <meta property="fc:frame:image:aspect_ratio" content="1:1" />
             ${buttonsHtml}
         </head>
         </html>
